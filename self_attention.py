d479eb57bee8fce296de14dd69d8de5fe3e29ce0
zhao meng <zhaom@zju.edu.cn>
2019-11-07 17:17:11 +0800

    Update self_attention.py

--- self_attention.py
+++ self_attention.py
@@ -4,15 +4,14 @@ import math
 from attention import BasicAttention
 
 class SelfAttention(BasicAttention):
-    def __init__(self, embd_size, q_k_hidden_size,v_hidden_size, head_size=1, score_func='scaled_dot', is_q=False,
-                 is_k=False, is_v=False):
-        q_embd_size=embd_size
-        k_embd_size=embd_size
-        v_embd_size=embd_size
-        super().__init__(q_embd_size, k_embd_size, v_embd_size, q_k_hidden_size,v_hidden_size, head_size, score_func, is_q, is_k, is_v)
+    def __init__(self, embd_size, num_heads=1, **kwargs):
+        q_embd_size = embd_size
+        k_embd_size = embd_size
+        v_embd_size = embd_size
+        super().__init__(q_embd_size, k_embd_size, v_embd_size, num_heads, **kwargs)
 
-    def forward(self, embd,mask=None):
-        q_embd=embd
-        k_embd=embd
-        v_embd=embd
-        return super().forward(q_embd, k_embd, v_embd,mask)
+    def forward(self, embd, mask=None):
+        q_embd = embd
+        k_embd = embd
+        v_embd = embd
+        return super().forward(q_embd, k_embd, v_embd, mask)
